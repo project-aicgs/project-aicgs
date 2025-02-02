8f1d5cdf3c8b4430eb3f3c2db20f790a1349b076
project-aicgs <projectaicgs@gmail.com>
2025-02-02 01:59:00 -0500

    Add aicgs netlify link to CORs

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://aicgs.netlify.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
